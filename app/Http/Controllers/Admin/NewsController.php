dfa9fd8401eb9caf52b2d20f5dbb75bca1693199
hirotaatamizu <hiro.music.is.life@gmail.com>
2019-05-31 17:33:38 +0900

    41

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -10,5 +10,11 @@ class NewsController extends Controller
   public function add()
     {
         return view('admin.news.create');
-    }    
+    }
+    
+    public function create(Request $request)
+  {
+      // admin/news/createにリダイレクトする
+      return redirect('admin/news/create');
+  }
 }
